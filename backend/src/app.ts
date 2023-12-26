b8a79bb62ae7646ab1844f3cda9a331b0564ac15
Radu Baltag <baltag.radu@gmail.com>
2023-12-26 13:04:04 +0200

    added routes and controllers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,13 @@
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
-import NoteModel from './models/note';
+import notesRoutes from './routes/notes';
 
 const app = express();
 
-app.get('/', async (req, res, next) => {
-  try {
-    const notes = await NoteModel.find().exec();
-    res.status(200).json(notes);
-  } catch (error) {
-    next(error);
-  }
-});
+app.use('/api/notes', notesRoutes);
 
 app.use((req, res, next) => {
-  next(Error('Endpoint not found'))
+  next(Error('Endpoint not found'));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
