488c1b2895c5ee33ec4433b6801725c032d0279a
Radu Baltag <baltag.radu@gmail.com>
2023-12-28 16:26:27 +0200

    notes styling with date

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Note as NoteModel } from './models/note';
+import styles from './styles/NotesPage.module.css';
 import Note from './components/Note';
-import { Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
@@ -23,9 +24,11 @@ function App() {
 
   return (
     <Container>
-      <Row xs={1} md={2} xl={3}>
+      <Row xs={1} md={2} xl={3} className='g-4'>
         {notes.map((note) => (
-          <Note note={note} key={note._id} />
+          <Col key={note._id}>
+            <Note note={note} className={styles.note} />
+          </Col>
         ))}
       </Row>
     </Container>
--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,19 +1,29 @@
 import styles from '../styles/Note.module.css';
 import { Card } from 'react-bootstrap';
 import { Note as NoteModel } from '../models/note';
+import { formatDate } from '../utils/formatDate';
 
 interface NoteProps {
   note: NoteModel;
+  className?: string;
 }
 
-const Note = ({ note }: NoteProps) => {
+const Note = ({ note, className }: NoteProps) => {
   const { title, text, createdAt, updatedAt } = note;
+
+  let dateText: string;
+  if (updatedAt > createdAt) {
+    dateText = 'Updated: ' + formatDate(updatedAt);
+  } else {
+    dateText = 'Created: ' + formatDate(createdAt);
+  }
   return (
-    <Card className={styles.noteCard}>
-      <Card.Body>
+    <Card className={`${styles.noteCard} ${className}`}>
+      <Card.Body className={styles.cardBody}>
         <Card.Title>{title}</Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
       </Card.Body>
+      <Card.Footer className='text-muted'>{dateText}</Card.Footer>
     </Card>
   );
 };
